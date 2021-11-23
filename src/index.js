8f920de3ff60e6b894d814b8695103bb5e996579
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-11-23 11:13:20 +1100

    Setup server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+// Initialize Express as an instance named "app".
+const app = express();  // a server instance
+
+// Separate these out in case we wanna use Docker or something to wrap the app.
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+// Best settings for setting up Express as an API server to receive and process JSON & form data.
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));  // for form data
+
+// Standard ExpressJS route, sends back a HTML response
+app.get('/', (request, response) => {
+
+    // response.send("Today, my life resumes...");
+
+    // Send response as json
+    response.json({message: "Today, my life resumes..."});
+});
+
+// API route, sends back a JSON response
+app.get('/json',(request, response) => {
+
+    response.json({'message':"Hello world!"})
+});
+
+
+// Run the server by making it 'listen' for network traffic
+app.listen(PORT, HOST, () => {
+    // Weird in-line conditional string interpolation to handle "0.0.0.0" -> "localhost" conversion
+    console.log(`Server listening at http://${HOST == "0.0.0.0" && "localhost"}:${PORT}/`);
+});
